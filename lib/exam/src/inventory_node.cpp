fccb2e21af97f3520cac1d006d0b4eb3551b0172
Richielee630 <60235970+Richielee630@users.noreply.github.com>
2020-03-24 17:30:41 -0700

    1.3

--- lib/exam/src/inventory_node.cpp
+++ lib/exam/src/inventory_node.cpp
@@ -1,6 +1,10 @@
 #include "../inc/inventory_node.h"
 
-inventory_node::inventory_node(int input_upc, std::string input_name, int input_count, int input_price, int date) {
+inventory_node::inventory_node(int input_upc, std::string input_name, int input_count, int input_price, int date):upc(input_upc),name(input_name),inventory_count(input_count),price() {
+    value_date node;
+    node.value = input_price;
+    node.date = date;
+    price.enqueue(node);
     next = nullptr;
 }
 
--- lib/exam/src/queue.cpp
+++ lib/exam/src/queue.cpp
@@ -1,31 +1,87 @@
 #include "../inc/queue.h"
 
-queue::queue() {
+queue::queue():queue_front(nullptr),queue_back(nullptr) {
 }
 
 queue::~queue() {
+    queue_node *next = queue_front;
+    while(next){
+        queue_node *cur = next;
+        next = next->next;
+        delete cur;
+    }
+    queue_front = nullptr;
+    queue_back = nullptr;
 }
 
 void queue::dequeue() {
-
+    if(queue_front){
+        queue_node* cur = queue_front;
+        queue_front = queue_front->next;
+        delete cur;
+        cur = nullptr;
+        if(!queue_front){
+            queue_back = queue_front;
+        }
+    }
 }
 
 void queue::enqueue(struct value_date input) {
-
+    queue_node* newnode = new queue_node(input);
+    if(queue_back){
+        queue_back->next = newnode;
+        queue_back = newnode;
+    }else{
+        queue_front = newnode;
+        queue_back = queue_front;
+    }
 }
 
 struct value_date queue::front() const {
-    return value_date{};
+    try{
+        return queue_front->data;
+
+    } catch(int i){
+        throw;
+        return value_date{};
+    }
 }
 
 struct value_date queue::back() const {
+    if(queue_back){
+        return queue_back->data;
+    }
     return value_date{};
 }
 
 bool queue::empty() const {
+    if(!queue_front){
+        return true;
+    }
     return false;
 }
 
 queue &queue::operator=(const queue &RHS) {
+    if(this != &RHS){
+        queue_node* next = queue_front;
+        while(next){
+            queue_node* cur = next;
+            next = next->next;
+            delete cur;
+            cur = nullptr;
+        }
+        queue_front = queue_back = nullptr;
+        if(!RHS.empty()){
+            queue_node* cur = RHS.queue_front;
+            queue_front = new queue_node(cur->data);
+            queue_back = queue_front;
+            cur = cur->next;
+            while(cur){
+                queue_back->next = new queue_node(cur->data);
+                cur = cur->next;
+                queue_back = queue_back->next;
+            }
+        }
+    }
     return *this;
 }
